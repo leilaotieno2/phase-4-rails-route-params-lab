5956c1b8622133d62cb266873d269522a0e7de9f
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-08-23 16:37:57 +0300

    first commit

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,23 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+
+    if params[:query].present?
+      query = params[:query].downcase
+      students = students.where('lower(first_name) LIKE ? OR lower(last_name) LIKE ?', "%#{query}%", "%#{query}%")
+    end
+
     render json: students
+  end
+
+  def grades
+    students = Student.order(grade: :desc).first
+    render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
